cfb880fb5588b25c651dda76b6e5e78ad7c7429e
Eduardo Soares <edurbs@gmail.com>
2024-12-18 16:55:14 -0300

    add user component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  standalone: true ,// angular 19 or higher it's the default
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent{}
