a96aa9a4acee44bdb6deab89b62f5463a6aa9a9f
Eduardo Soares <edurbs@gmail.com>
2025-01-02 11:44:06 -0300

    add: type function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,9 +10,9 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
-  // select = output<string>();
+  @Output() select = new EventEmitter<string>();
 
+  // select = output<string>();
   // avatar = input.required<string>();
   // name = input.required<string>();
   // imagePath = computed(() => {
