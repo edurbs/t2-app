49f61750c51dcd0d37c54e0b89fefced2395599a
Eduardo Soares <edurbs@gmail.com>
2025-01-05 22:45:22 -0300

    add: selected users

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -8,6 +8,33 @@ import { TaskComponent } from "./task/task.component";
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input() name: string | undefined;
+  @Input({required:true}) userId!: string;
+  @Input({required: true}) name!: string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Lean all about Angular',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Master Java',
+      summary: 'Lean all about Java',
+      dueDate: '2025-06-31',
+    },
+    {
+      id: 't3',
+      userId: 'u4',
+      title: 'Master Typescript',
+      summary: 'Lean all about Typescript',
+      dueDate: '2024-06-31',
+    }
+  ];
 
+  get selectedUserTasks(){
+    return this.tasks.filter((tasks)=>tasks.userId==this.userId);
+  }
 }
